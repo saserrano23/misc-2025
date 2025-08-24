ced5c6fe1e097c7307f2aa42f1cbd31ee8880264
saserrano23 <saserrano@comcast.net>
2025-08-23 22:50:13 -0400

    initial aggregation file

---
+++ file0.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import numpy as np
+
+
+def summarize(df):
+
+    df = df.groupby(['city']).aggregate({'score': 'mean'}).reset_index()
+
+    return df
+
+
+city = ['BOS','BOS','BOS','NYC','NYC','NYC','CHI','CHI','CHI']
+score = [50, 95, 82, 70, 70, 72, 38, 98, 55]
+
+df = pd.DataFrame(zip(city, score), columns = ['city','score'])
+
+df_sum = summarize(df)
+
+print('summary df: ', df_sum)
